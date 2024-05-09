3f1878c56e07d38ccf5e5ee5c1fbcee696f224ef
jmosquerarei <jamesmosqr@gmail.com>
2024-05-09 00:08:58 -0500

    x

---
+++ 01-introPHP/1.php
@@ -0,0 +1,10 @@
+<html>
+    <head></head>
+    <body>
+        <h1>Mi primer fichero en PHP</h1>
+    </body>
+</html>
+<!--en este apartado podemos visualizar la creacion de elementos basicos en el lenguaje de programacion
+como se ve, no hay datos del lenguaje de programacion, solo el archivo esta en formato php, lo demas es html,
+por lo tanto php, puede procesar datos de tipo html
+-->
