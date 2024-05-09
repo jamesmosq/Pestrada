112ef78954071b7f325edd8df03e5f333bb6389f
jmosquerarei <jamesmosqr@gmail.com>
2024-05-09 00:11:59 -0500

    x

---
+++ 01-introPHP/2.php
@@ -0,0 +1,13 @@
+<html>
+<head></head>
+<body>
+<h1>Mi primer fichero en PHP</h1>
+<?php echo 'Y funciona perfecto!'; ?>
+</body>
+</html>
+
+<!--Para añadir tus scripts de PHP debes usar --><?php //?><!-- o --><?// ?><!--
+(esta última debes activarla en la configuración).-->
+
+<!--Toda sentencia, la instrucción, debe terminar con un ;. Independientemente de donde se abra o se cierre el script. Eres libre de
+añadir tabulaciones y saltos de línea para dejarlo más ordenado.-->
